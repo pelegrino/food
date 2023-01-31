ceac5f1829a3b5bd697a3ce752403c29b4b49050
pelegrino <pelegrino@gmail.com>
2023-01-31 13:14:50 -0300

    Login1

--- src/main/java/br/com/pelegrino/food/infraestructure/web/controller/LoginController.java
+++ src/main/java/br/com/pelegrino/food/infraestructure/web/controller/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-	@GetMapping
-	private String login;
+	@GetMapping(path = { "/login", "/" })
+	private String login() {
+		return "login";
+	}
 }
