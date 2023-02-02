c0010b1940bebae36980d67ea62803a75ffe287a
pelegrino <pelegrino@gmail.com>
2023-02-01 22:07:31 -0300

    Mapeando a entidade para o banco de dados

--- src/main/java/br/com/pelegrino/food/domain/cliente/Cliente.java
+++ src/main/java/br/com/pelegrino/food/domain/cliente/Cliente.java
@@ -1,11 +1,16 @@
 package br.com.pelegrino.food.domain.cliente;
 
 import br.com.pelegrino.food.domain.usuario.Usuario;
+import jakarta.persistence.Entity;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@Entity
+@SuppressWarnings("serial")
 public class Cliente extends Usuario {
 
 	private String cpf;
--- src/main/java/br/com/pelegrino/food/domain/usuario/Usuario.java
+++ src/main/java/br/com/pelegrino/food/domain/usuario/Usuario.java
@@ -1,5 +1,9 @@
 package br.com.pelegrino.food.domain.usuario;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,12 +11,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Usuario {
+@SuppressWarnings("serial")
+@MappedSuperclass
+public class Usuario implements Serializable {
 	
+	@EqualsAndHashCode.Include
+	@Id
 	private Integer id;
+	
 	private String nome;
+	
 	private String email;
+	
 	private String senha;
+	
 	private String telefone;
 
 }
