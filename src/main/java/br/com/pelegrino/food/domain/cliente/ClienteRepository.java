4840fe162ca08ee108a25a61bab3842d55b33ec6
pelegrino <pelegrino@gmail.com>
2023-02-07 21:54:09 -0300

    Arrumado e Corrigido

---
+++ src/main/java/br/com/pelegrino/food/domain/cliente/ClienteRepository.java
@@ -0,0 +1,8 @@
+package br.com.pelegrino.food.domain.cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	
+}
--- src/main/java/br/com/pelegrino/food/infraestructure/web/controller/PublicController.java
+++ src/main/java/br/com/pelegrino/food/infraestructure/web/controller/PublicController.java
@@ -1,10 +1,14 @@
 package br.com.pelegrino.food.infraestructure.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.pelegrino.food.application.ClienteService;
 import br.com.pelegrino.food.domain.cliente.Cliente;
 
 
@@ -12,21 +16,20 @@ import br.com.pelegrino.food.domain.cliente.Cliente;
 @RequestMapping(path = "/public")
 public class PublicController {
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	@GetMapping("/cliente/new")
 	public String newCliente(Model model) {
 		model.addAttribute("cliente", new Cliente());
-<<<<<<< HEAD
-		return "clienteCadatro";
-=======
-		return "cliente-cadastro";
+		return "clienteCadastro";
 	}
 	
 	@PostMapping(path = "/cliente/save")
 	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
 		clienteService.saveCliente(cliente);
 		
-		return "cliente-cadastro";
->>>>>>> d9e0613 (Persistência no MySQL)
+		return "clienteCadastro";
 	}
 
 }
