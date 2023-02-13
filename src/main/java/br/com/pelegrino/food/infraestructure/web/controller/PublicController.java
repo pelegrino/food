620485db3ca81b7c81a84cbadca8af8ed19b94b3
pelegrino <pelegrino@gmail.com>
2023-02-13 20:03:28 -0300

    Validando os campos do cliente

--- src/main/java/br/com/pelegrino/food/infraestructure/web/controller/PublicController.java
+++ src/main/java/br/com/pelegrino/food/infraestructure/web/controller/PublicController.java
@@ -3,6 +3,7 @@ package br.com.pelegrino.food.infraestructure.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.pelegrino.food.application.ClienteService;
 import br.com.pelegrino.food.domain.cliente.Cliente;
+import jakarta.validation.Valid;
 
 
 @Controller
@@ -22,14 +24,21 @@ public class PublicController {
 	@GetMapping("/cliente/new")
 	public String newCliente(Model model) {
 		model.addAttribute("cliente", new Cliente());
-		ControllerHelper.setEditMode(model, true);
+		ControllerHelper.setEditMode(model, false);
 		return "clienteCadastro";
 	}
 	
 	@PostMapping(path = "/cliente/save")
-	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
-		clienteService.saveCliente(cliente);
+	public String saveCliente(@ModelAttribute("cliente") @Valid Cliente cliente,
+			Errors errors,
+			Model model) {
 		
+		if (!errors.hasErrors()) {
+			clienteService.saveCliente(cliente);
+			model.addAttribute("msg", "Cliente salvo com sucesso");
+		} 
+		
+		ControllerHelper.setEditMode(model, false);
 		return "clienteCadastro";
 	}
 
