a307c8d9177e9a2424c4266af668766c16e77451
Pelegrino <pelegrino@gmail.com>
2023-02-22 14:53:35 -0300

    WebSecurityConfigurerAdapter - Erro

--- src/main/java/br/com/pelegrino/food/infraestructure/web/security/SecurityConfig.java
+++ src/main/java/br/com/pelegrino/food/infraestructure/web/security/SecurityConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,10 +19,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
-			.authorizeHttpRequests()
-			.requestMatchers("/images/**", "/css/**", "/js/**", "/public/", "/sbpay").permitAll()
-			.requestMatchers("/cliente/**").hasRole(Role.CLIENTE.toString())
-			.requestMatchers("/restaurante/**").hasRole(Role.RESTAURANTE.toString())
+			.authorizeRequests()
+			.antMatchers("/images/**", "/css/**", "/js/**", "/public/", "/sbpay").permitAll()
+			.antMatchers("/cliente/**").hasRole(Role.CLIENTE.toString())
+			.antMatchers("/restaurante/**").hasRole(Role.RESTAURANTE.toString())
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
