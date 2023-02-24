0b6a0017252b6d0da013cd8fdf679e1674ffc1eb
Pelegrino <pelegrino@gmail.com>
2023-02-24 14:25:37 -0300

    Mostrando o usuário logado no cabeçalho da página

--- src/main/java/br/com/pelegrino/food/infraestructure/web/security/SecurityConfig.java
+++ src/main/java/br/com/pelegrino/food/infraestructure/web/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 			.authorizeRequests()
-			.antMatchers("/images/**", "/css/**", "/js/**", "/public/", "/sbpay").permitAll()
+			.antMatchers("/images/**", "/css/**", "/js/**", "/public/**", "/sbpay").permitAll()
 			.antMatchers("/cliente/**").hasRole(Role.CLIENTE.toString())
 			.antMatchers("/restaurante/**").hasRole(Role.RESTAURANTE.toString())
 			.anyRequest().authenticated()
